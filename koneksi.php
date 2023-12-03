5a1a5672a462a0ff3cb88c34578ff3702a5966db
Komang Kurniawan W <komang.kurniawan@outlook.com>
2023-12-03 13:05:34 +0800

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -5,10 +5,4 @@ $username = "root";
 $password = "root";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// echo "Connected successfully";
-
 ?>
